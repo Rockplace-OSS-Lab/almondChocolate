a43c5a12456c5f5fb4ee1d2a272aa4330542d6b2
javajigi <javajigi@gmail.com>
2017-03-16 10:39:05 +0900

    refactor: remove imageTags option

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-APP_NAME=almondchocolate
-
-COMMIT_HASH="$(git show-ref --head | grep -h HEAD | cut -d':' -f2 | head -n 1 | head -c 10)"
-
-./gradlew clean build buildDocker -x test -DdockerImageTags=$APP_NAME_$COMMIT_HASH
+./gradlew clean build buildDocker -x test
 
 docker-compose up --build -d
